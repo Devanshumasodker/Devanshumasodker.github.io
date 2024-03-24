e5d5406f0037c1c751a204509c0f248cfc72d1af
Devanshu Masodker <devanshumasodker357@gmail.com>
2024-03-24 23:54:56 +0530

    configuration-updatted

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ var data = [
     { icon: "fa-brands fa-linkedin", url: "https://www.linkedin.com/in/devanshu-masodker-624782227/", text: "LinkedIn"},
     { icon: "fa-brands fa-github", url: "https://github.com/Devanshumasodker", text: "GitHub"},
     { icon: "fa-brands fa-twitter", url: "https://twitter.com/DevanshuMehra29?t=yie5Ef4Yw1RKX3HFpYwurg&s=09" , text: "Twitter"},
-    { icon: "fa-solid fa-briefcase", url: "http://devanshumasodker.me/devanshu-portfolio/", text: "Portfolio"},
+    { icon: "fa-solid fa-briefcase", url: "https://devanshumasodker.me/devanshu-portfolio/", text: "Portfolio"},
 ]
 var links = document.querySelectorAll(".image .links");
 
